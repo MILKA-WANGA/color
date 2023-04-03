4d41e8c350dec87dd5424e368f91dab62c49f783
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-03 02:02:35 -0700

    Add files via upload

---
+++ ColorValue.java
@@ -0,0 +1,20 @@
+public class ColorValue
+{
+    public static void main(String[]args)
+    {
+        char color='r';
+        switch(color)
+        {
+            case 'g':System.out.println("green");
+            break;
+             case 'b':System.out.println("blue");
+            break;
+             
+            case 'r':System.out.println("red");
+            break;
+            
+            default:System.out.println("It is not a color");
+            
+        }
+    }
+}
